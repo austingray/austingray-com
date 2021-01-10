97bd7c17e9de472f7bb60fce32e23b4e5a65630e
Austin Gray <waustingray@gmail.com>
2021-01-09 17:17:16 -0700

    centered text

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 const Banner = () => (
-  <div className="flex z-10 relative text-xl text-center bg-white rounded-lg p-4">
+  <div className="z-10 relative text-xl text-center bg-white rounded-lg p-4">
     Hello, my name is Austin. This site is under construction. Come back soon!
   </div>
 );
